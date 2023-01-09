057c464d29e0c174f5dc1eeb3ad2b1d500ced597
MaksShostak <107349913+MaksShostak@users.noreply.github.com>
2023-01-09 17:02:50 +0200

    add pagination and search to news

--- controllers/news/getNews.js
+++ controllers/news/getNews.js
@@ -1,9 +1,17 @@
 const News = require("../../models/newsSchema");
 
-const getNews = async (req, res, next) => {
-  const news = await News.find();
-
-  res.status(200).json(news);
+const getNews = async (req, res) => {
+  const { page = 1, limit = 9, search = "" } = req.query;
+  const skip = (page - 1) * limit;
+  if (search === "") {
+    const news = await News.find({}).skip(skip).limit(limit);
+    res.status(200).json(news);
+  } else {
+    const news = await News.find({ $text: { $search: search } })
+      .skip(skip)
+      .limit(limit);
+    res.status(200).json(news);
+  }
 };
 
 module.exports = getNews;
