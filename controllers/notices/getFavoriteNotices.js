b5803195e9e3e21fc0be4513f518bdc18217b621
MaksShostak <107349913+MaksShostak@users.noreply.github.com>
2023-01-10 21:15:44 +0200

    did pagination to users all notices and favorite

--- controllers/notices/getFavoriteNotices.js
+++ controllers/notices/getFavoriteNotices.js
@@ -2,12 +2,18 @@ const { User } = require("../../models/userSchema");
 
 const getFavoriteNotices = async (req, res) => {
   const { _id } = req.user;
+  const { page = 1, limit = 3 } = req.query;
+  const skip = (page - 1) * limit;
 
   const result = await User.findOne({ _id }, "favorite").populate({
     path: "favorite",
     select:
       "category title name birthday breed sex location price imageURL comments",
+    // perDocumentLimit: limit,
+    // skip,
+    options: { limit, skip },
   });
+
   res.status(200).json(result.favorite.reverse());
 };
 
