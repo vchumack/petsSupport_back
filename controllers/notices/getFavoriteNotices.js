d8c3ddc7df6df35fb95b083060421cd865db97be
MaksShostak <107349913+MaksShostak@users.noreply.github.com>
2023-01-13 12:32:11 +0200

    fix

--- controllers/notices/getFavoriteNotices.js
+++ controllers/notices/getFavoriteNotices.js
@@ -8,7 +8,7 @@ const getFavoriteNotices = async (req, res) => {
     const result = await User.findOne({ _id }, "favorite").populate({
       path: "favorite",
       select:
-        "category title name birthday breed sex location price imageURL comments owner",
+        "category title name birthday breed sex location price imageURL comments owner updatedAt",
       // perDocumentLimit: limit,
       // skip,
       options: { limit, skip },
